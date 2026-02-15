b3135f4506d1cdb96f5f66f0727799114eae66f8
eityroy <eityroy027@gmail.com>
2026-02-15 12:36:08 +0600

    react practice1

--- react-first-project/src/App.jsx
+++ react-first-project/src/App.jsx
@@ -1,7 +1,11 @@
 
 
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './Countries/Countries'
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+.then( res => res.json())
 
 function App() {
   
@@ -9,7 +13,11 @@ function App() {
   return (
     <>
     
-  <Countries></Countries>
+  <Suspense fallback= {<p>The page is loading......</p>}>
+    <Countries countriesPromise={countriesPromise}>
+
+    </Countries>
+  </Suspense>
 
     </>
   )
