4eb11345cfc1aeb3c3d91f56602c3f9a577b0468
eityroy <eityroy027@gmail.com>
2026-02-15 14:32:53 +0600

    react project1

--- react-first-project/src/Component/Country/Country.jsx
+++ react-first-project/src/Component/Country/Country.jsx
@@ -8,9 +8,11 @@ const Country = ({country}) => {
             <img style={{height:'150px', width:'150px'}} src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2>Name:{country.name.common}</h2>
             <p>Capital: {country.capital.capital}</p>
-            <p>Area: {country.area.area}</p>
             <p>Region: {country.region.region}</p>
             <p>languages: {country.languages.languages.eng}</p>
+            {/* conditions */}
+            <p>Population: {country.population.population} { country.population.population >200000 ? "High Population" : "Low Population" }</p>
+            <p>Area: {country.area.area} { country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
         </div>
     );
 };
